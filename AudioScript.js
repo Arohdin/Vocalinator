6c34ba71d55536dad610664ad168a63a472164d4
MrZtone <olasteen@c-112-60.eduroam.liu.se>
2016-09-08 15:19:34 +0200

    Frekvenssaker

---
+++ AudioScript.js
@@ -0,0 +1,74 @@
+  
+ 
+  navigator.getUserMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
+
+
+window.AudioContext = window.AudioContext ||
+                      window.webkitAudioContext;
+
+var context = new AudioContext();
+ var analyzer = context.createAnalyser();
+ analyzer.fftSize = 512;
+ var amplitudeArray = new Uint8Array(analyzer.frequencyBinCount);
+
+var sampleRate= context.sampleRate;
+var frequencyArray= new Float32Array(analyzer.frequencyBinCount);
+
+for(var i =0; i<analyzer.frequencyBinCount; ++i)
+{
+	frequencyArray[i]= i*(sampleRate/analyzer.frequencyBinCount)
+}
+
+ var volumeNode = context.createGain();
+ volumeNode.gain.value = 0.0;
+
+  var errorCallback = function(e) {
+    console.log('Reeeejected!', e);
+  };
+
+navigator.getUserMedia({audio: true}, function(stream) {
+ microphone = context.createMediaStreamSource(stream);
+
+  microphone.connect(analyzer);
+  analyzer.connect(volumeNode);
+  volumeNode.connect(context.destination);
+}, errorCallback);
+
+setInterval(function(){
+	analyzer.getByteFrequencyData(amplitudeArray);
+	drawBasic()}, 33);
+
+
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+
+function drawBasic() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'X');
+      data.addColumn('number', 'Dogs');
+
+      for(var i =0;i<analyzer.frequencyBinCount;++i)
+      {
+      	data.addRow([frequencyArray[i],amplitudeArray[i]]);
+      }
+
+      var options = {
+        hAxis: {
+          title: 'Time'
+        },
+        vAxis: {
+          title: 'Popularity'
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
+
+
